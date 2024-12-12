0517e8dbdd2fdc469c83b86bf483061fff54ff7c
fatowl-dev <kingyo.summer.japan@gmail.com>
2024-12-13 06:17:00 +0900

    Initial Commit

---
+++ XcodeGit/XcodeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitApp.swift
+//  XcodeGit
+//
+//  Created by fatowl on 2024/12/13.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
